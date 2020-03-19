335f8ea83ca3450a1c1df63d98f517ddaad7be00
Marcelo Zapatta <marcelo@olivasdigital.com.br>
2020-03-18 21:40:48 -0300

    Operações

---
+++ Atividade3/operacoes.js
@@ -0,0 +1,16 @@
+var numero1, numero2;
+
+numero1 = prompt('Insira o primeiro número:');
+numero2 = prompt('Insira o segundo número: ');
+
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var produto = numero1 * numero2;
+var divisao = numero1 / numero2;
+var resto = numero1 % numero2;
+
+alert('Soma: ' + soma);
+alert('Subtração: ' + subtracao);
+alert('Produto: ' + produto);
+alert('Divisão: ' + divisao);
+alert('Resto: ' + resto);
