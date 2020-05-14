fcff9c3192d415cf6ed9ed7b44bdc1cb5f3e5308
Marcelo <marcelozapatta0@gmail.com>
2020-05-13 21:31:31 -0300

    Finalização atividade 4

--- Atividade4/index.js
+++ Atividade4/index.js
@@ -1,1 +1,57 @@
-var escolhaInput = prompt('Escolha uma opção: \n\n[1] Pedra\n\n[2] Papel\n\n[3] Tesoura');
+var escolhaInput = prompt(
+  "Escolha uma opção: \n\n[1] Pedra\n\n[2] Papel\n\n[3] Tesoura"
+);
+
+var escolhaUsuario = "";
+
+switch (escolhaInput) {
+  case "1":
+    escolhaUsuario = "PEDRA";
+    break;
+  case "2":
+    escolhaUsuario = "PAPEL";
+    break;
+  default:
+    escolhaUsuario = "TESOURA";
+    break;
+}
+
+var randomComputador = Math.random();
+
+var escolhaComputador = "PAPEL";
+
+if (randomComputador >= 0 && randomComputador < 0.33) {
+  escolhaComputador = "PEDRA";
+} else if (randomComputador >= 0.33 && randomComputador < 0.66) {
+  escolhaComputador = "TESOURA";
+}
+
+alert(
+  "Escolha computador: " +
+    escolhaComputador +
+    "\nSua escolha: " +
+    escolhaUsuario
+);
+
+var ganhador = "";
+
+if (escolhaUsuario === escolhaComputador) {
+  ganhador = "EMPATE";
+} else if (escolhaUsuario === "PAPEL") {
+  ganhador = "USUARIO";
+  if (escolhaComputador === "TESOURA") {
+    ganhador = "COMPUTADOR";
+  }
+} else if (escolhaUsuario === "PEDRA") {
+  ganhador = "USUARIO";
+  if (escolhaComputador === "PAPEL") {
+    ganhador = "COMPUTADOR";
+  }
+} else {
+  ganhador = "USUARIO";
+  if (escolhaComputador === "PEDRA") {
+    ganhador = "COMPUTADOR";
+  }
+}
+
+alert("Resultado: " + ganhador);
