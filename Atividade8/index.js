5bb7ad62b481ed6f414c25e315ee72d0fee3c051
Marcelo <marcelozapatta0@gmail.com>
2020-05-27 21:18:33 -0300

    Atividade 8

---
+++ Atividade8/index.js
@@ -0,0 +1,17 @@
+const imagemJanela = document.getElementById('imagem-janela');
+
+function fecharJanela () {
+  imagemJanela.src = 'assets/img/janelafechada.jpg';
+}
+
+function abrirJanela() {
+  imagemJanela.src = 'assets/img/janelaaberta.png';
+}
+
+function quebrarJanela() {
+  imagemJanela.src = 'assets/img/janelaquebrada.png';
+}
+
+imagemJanela.addEventListener('mouseenter', abrirJanela);
+imagemJanela.addEventListener('mouseleave', fecharJanela);
+imagemJanela.addEventListener('click', quebrarJanela);
